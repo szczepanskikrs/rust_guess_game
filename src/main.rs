39590e4e3b45adced4daa95e8c4d8fe25e26671f
szczepanskikrs <szczepanskikrs@gmail.com>
2020-10-22 19:23:40 +0200

    Add looping and guessing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
+use std::cmp::Ordering;
 use std::io;
+
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess");
-    let mut guessed = String::new();
-    io::stdin()
-        .read_line(&mut guessed)
-        .expect("Failed to read line");
 
-    println!("You guessed: {}",guessed)
+    let secret = rand::thread_rng().gen_range(1, 101);
+    loop {
+        println!("Please input your guess");
+
+        let mut guessed = String::new();
+
+        io::stdin()
+            .read_line(&mut guessed)
+            .expect("Failed to read line");
+
+        let guessed_value: u32 = match guessed.trim().parse() {
+            Ok(number) => number,
+            Err(_) => continue
+        };
+
+        match guessed_value.cmp(&secret) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too large"),
+            Ordering::Equal => {
+                println!("This is it!");
+                break;
+            }
+        }
+    }
 }
